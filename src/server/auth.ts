b226d9c942ae0b233b672ab3c4a3ffa47b986e2d
Gonzalo Caballero <gonzalocaballero@me.com>
2023-11-12 17:22:36 -0300

    feat(auth): add credentials log in

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,11 +1,11 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { getServerSession, type DefaultSession, type NextAuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import { getServerSession, type DefaultSession, type NextAuthOptions, type Awaitable } from 'next-auth';
 import type { UserRole } from '@prisma/client';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
-import { env } from '~/env.mjs';
 import { db } from '~/server/db';
+import type { AdapterUser } from 'next-auth/adapters';
 
 declare module 'next-auth' {
   interface Session extends DefaultSession {
@@ -21,20 +21,30 @@ declare module 'next-auth' {
 }
 
 export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt',
+  },
   callbacks: {
-    session: ({ session, user }) => ({
+    session: ({ session, token }) => ({
       ...session,
       user: {
         ...session.user,
-        id: user.id,
+        id: parseInt(token.sub!) as unknown as string,
       },
     }),
   },
   adapter: PrismaAdapter(db),
   providers: [
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize: async (credentials) => {
+        const user = await db.user.findUnique({ where: { email: credentials!.email, password: credentials!.password, enabled: true } });
+        return user as Awaitable<AdapterUser> | null;
+      },
     }),
   ],
 };
