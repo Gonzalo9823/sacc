c6e8e601dbf5d550791f853ff7286f19dd0d0f04
Gonzalo Caballero <gonzalocaballero@me.com>
2023-11-12 15:47:30 -0300

    feat(router/post): remove router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,6 @@
-import { postRouter } from '~/server/api/routers/post';
 import { createTRPCRouter } from '~/server/api/trpc';
 
 export const appRouter = createTRPCRouter({
-  post: postRouter,
 });
 
 export type AppRouter = typeof appRouter;
