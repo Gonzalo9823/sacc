6979a8b6997845a8277aaef5b83fa4d4f6a68b46
Gonzalo Caballero <gonzalocaballero@me.com>
2023-11-12 17:22:49 -0300

    feat(routers/auth): add auth route

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,7 +1,10 @@
 import { stationRouter } from '~/server/api/routers/station';
+import { authRouter } from '~/server/api/routers/auth';
+
 import { createTRPCRouter } from '~/server/api/trpc';
 
 export const appRouter = createTRPCRouter({
+  auth: authRouter,
   station: stationRouter,
 });
 
