f8cd35a4a81aedcfd3fc19a5199f3f6cc477e0fd
Gonzalo Caballero <gonzalocaballero@me.com>
2023-11-27 15:35:11 -0300

    chore(deps): add react hook form, react hot toast and tailwind form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,15 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { type Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ['var(--font-sans)', ...fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [forms],
 } satisfies Config;
