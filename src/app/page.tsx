0244b715abb8a2dfedbc73f71431f73914676b4b
Gonzalo Caballero <gonzalocaballero@me.com>
2023-11-27 15:36:11 -0300

    feat(page): add sign in form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,34 @@
-// Create root component
-// Path: src/app/page.tsx
-// Compare this snippet from src/app/signIn/page.tsx:
-'use client';
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-
-
 import { getServerAuthSession } from '~/server/auth';
-
-
+import SignInForm from '~/app/_components/sign-in-form';
 
 export default async function Home() {
-    const session = await getServerAuthSession();
-    
-    if (session) {
-        return (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-         {/* <UserSignIn />  */}
-        </main>
-        );
-    }
-    
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-         {/* <UserSignIn /> */}
+  const session = await getServerAuthSession();
+  if (session) redirect('/dashboard');
 
-            <Link href="/signUp">
-                Sign Up
-            </Link>
-        </main>
-    );
+  return (
+    <div className="flex min-h-full flex-1">
+      <div className="flex flex-1 flex-col px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+        <div className="mx-auto w-full max-w-sm lg:w-96">
+          <div>
+            <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">Iniciar Sesión</h2>
+          </div>
+          <div className="mt-10">
+            <div>
+              <SignInForm />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden w-0 flex-1 lg:block">
+        <img
+          className="absolute inset-0 h-full w-full object-cover"
+          src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
