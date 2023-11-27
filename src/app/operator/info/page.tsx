ae771d58c603275fa7d0fc7a9978eefb00d0b30c
ccatavergara <mcvergara1@miuandes.cl>
2023-11-27 19:49:39 -0300

    feat:frontend

--- src/app/operator/info/page.tsx
+++ src/app/operator/info/page.tsx
@@ -1,8 +1,7 @@
 import { redirect } from 'next/navigation';
-import Link from 'next/link';
 import { getServerAuthSession } from '~/server/auth';
 import Image from 'next/image';
-
+import Link from 'next/link';
 
 import './styles.css';
 import SignInForm from '~/app/_components/sign-in-form';
@@ -17,34 +16,30 @@ export default async function Home() {
     <div className="flex min-h-full flex-1">
       <div className="flex flex-1 flex-col px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
       <div className="logo-container lg:block">
+        <Link href='/operator' passHref>
         <Image
           className="logo-image"
           src={StationLogo}
           alt=""
         />
+        </Link>
       </div>
         <div className="mx-auto w-full max-w-sm lg:w-96">
           <div>
-            <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">Bienvenido a SACC Estaciones de Entrega</h2>
+            <h2 className="mt-10 text-2xl font-bold leading-9 tracking-tight text-gray-900">Ingresa el PAQUETE</h2>
           </div>
-          <div className="mt-10">
-          <Link href="/operator" passHref>
-            <button className="button">
-              Soy Operario
-            </button>
-          </Link>
-          <Link href="/client" passHref>
-            <button className="button">
-              Soy Cliente
-            </button>
-          </Link>
+          <div>
+            <h2 className='pass-subtitle'>
+              El casillero es:
+            </h2>
+            <h1 className='pass-title'>Aca dato casillero</h1>
           </div>
         </div>
-      <div className="mt-auto">
-        <Link href="/signIn" passHref>
-          <button className='button-signin'>Sign In for Admins</button>
+        <Link href="/" passHref className='mt-2'>
+        <button className="button-signin">
+          Cerrar Sesion
+        </button>
         </Link>
-      </div>
       </div>
       <div className="relative hidden w-0 flex-1 lg:block">
         <Image className="absolute inset-0 h-full w-full object-cover" src={SignInImage} alt="" />
