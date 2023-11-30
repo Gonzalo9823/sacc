16fe5f4f16c88410c601dc5c71f30487bdd51d96
Gonzalo Caballero <gonzalocaballero@me.com>
2023-11-30 09:09:55 -0300

    feat(AdminLayout): add back button

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -23,7 +23,13 @@ export default async function LoggedLayout({ children }: { children: React.React
               <NavbarOptions />
             </div>
 
-            <div className="flex items-center">
+            <div className="flex items-center space-x-4">
+              <Link
+                href="/"
+                className="flex h-10 items-center justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 disabled:bg-gray-100"
+              >
+                Pagina Principal
+              </Link>
               <NavBarSignOutButton />
             </div>
           </div>
